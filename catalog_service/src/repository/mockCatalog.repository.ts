62f7e787f18f9a8a43b3776b549eb74e7f5a72cd
sandeep-simplify <sandeep@simplifymoney.in>
2024-05-19 01:00:12 +0530

    added TDD test libraries and 1 catalogService test

---
+++ catalog_service/src/repository/mockCatalog.repository.ts
@@ -0,0 +1,28 @@
+import { ICatalogRepository } from "../interface/catalogRepository.interface";
+import { Product } from "../models/product.model";
+
+export class MockCatalogRespository implements ICatalogRepository {
+    create(data: Product): Promise<Product> {
+        const mockProduct = {
+            ...data,
+            id: 123,
+        } as Product;
+        return Promise.resolve(mockProduct);
+    }
+
+    update(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+
+    delete(id: any) {
+        throw new Error("Method not implemented.");
+    }
+
+    find(): Promise<Product[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    findOne(id: number): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+}
